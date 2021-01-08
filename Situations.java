fbeab724c64c1b815545a4656f70dd2c0fd2c346
Halila2727 <71394814+Halila2727@users.noreply.github.com>
2021-01-08 03:03:03 -0500

    ( ˘︹˘ )  (͠≖ ͜ʖ͠≖)👌
    :) or :(  ?
    None.
    :|

--- Situations.java
+++ Situations.java
@@ -2,87 +2,93 @@ public class Situations extends TheGame {
     private String iIncident;
     private String character;
 
-
-    static String attractiveUglyPersonAsksYouOut = "Here you are, sitting at the library. You really seem to enjoy that dictionary. But then\n"
-            + " you notice a person approaching you. Be wary, as this person is prone to fits of rage and yelling. The person asks you out.\n"
+//Don't put \n at the end of the strings. It looks ugly whoever keeps editing in the \n in the situations. This version has the 
+//right amount, don't delete or add anymore \n. I am serious
+   
+    static String attractiveUglyPersonAsksYouOut = "Here you are, sitting at the library. You really seem to enjoy that dictionary. But then "
+            + " you notice a person approaching you. Be wary, as this person is prone to fits of rage and yelling. The person asks you out. "
             + " You appreciate the gesture, but you don't feel the same way. You say:\n1.\"Yes, I've been waiting for you to ask me that forever.\""
             + "\n2.\"You are a great and lovely person but we just are not meant to be.\"\n3.\"I'm going to be honest. We are not compatible.\"";
-    //2nd situation
+    
     String attractiveUPAYO1 = "That was a terrible choice. Now, you have spent 2 miserable weeks with a person you have absolutely no interest in.";
-    String attAns1 = "You decide to break up with them. But how?\n1. You treat the person to a nice "
-            + "3-course meal. At the end, you say all the usual break-up \"It's not you, it's me and blah blah....\" and you break up!"
+    String attAns1 = "You decide to break up with them. But how?\n1. You treat the person to a nice 3-course meal. At the end, you say all the "
+            + "usual break-up \"It's not you, it's me and blah blah....\" and you break up!"
             + "\n2. Hey, it is December 24th! So, if you break up now, over text, they'll get over it by the time schools open up and "
             + "you see them again!\n3. Meet with them face-to-face and tell them straight up that you will not date them.";
-    //Also 2nd situation
-    String attractiveUPAYO2 = "Now, that's what I call a pro. You have mastered the skill of smooth-talking. The person took it well.\n"
-            + "The truth is, you only like one person in the entire school. Now, with the extra ego that came with the rejection, you decide\n"
-            + "to ask out the person you like. How do you do it?\n1. You ask them outright. \n2. You become their friend and try to drop subtle"
-            + "hints while talking to them.\n3. You kiss them outright and you ask \"I would love to date you\" while pointing at "
-            + "yourself.";
-    //Also 2nd situation
-    String attractiveUPAYO3 = "You idiot. You didn't know at the time, but this person was a HUUUUGE social media influencer with 2,345\n"
+    
+    String attractiveUPAYO2 = "Now, that's what I call a pro. You have mastered the skill of smooth-talking. The person took it well. "
+            + "The truth is, you only like one person in the entire school. Now, with the extra ego that came with the rejection, you decide "
+            + "to ask out the person you like. How do you do it?\n1. You ask them outright. \n2. You become their friend and try to drop subtle "
+            + "hints while talking to them.\n3. You blow a kiss and ask \"How about a date\"?";
+    
+    String attractiveUPAYO3 = "You idiot. You didn't know at the time, but this person was a HUUUUGE social media influencer with 2,345 "
             + "FOLLOWERS. That's more than the entire graduating class! They also have 18 best friends! Now, they are coming for you.";
     String attAns2 =  "Your DMs are full of clown emojis! You act like you don't care but really, you are deeply hurt :( "
             + "\nWhat do you do to stop this?\n1. Make a public apology and say you were wrong.\n2. Start posting in "
             + "Cuneiform, the earliest known system of writing. \n3. Call you representative or senator and ask for them to vouch for you.";
-    //3rd situation
-    String attractiveUPAYO11 = "HAHAHAHA! They got a good laugh out of that one! The person thought you were joking! Seriously, who spends $80\n"
-            + "just to break up. I am dying of laughter! If only, I was as attractive as you. At least I am not as dumb. HAHAHHA! Now you are in a\n"
+    
+    String attractiveUPAYO11 = "HAHAHAHA! They got a good laugh out of that one! The person thought you were joking! Seriously, who spends $80 "
+            + "just to break up. I am dying of laughter! If only, I was as attractive as you. At least I am not as dumb. HAHAHHA! Now you are in a "
             + "bit of a pickle.";
-    String attAns3 = "Alright, I'll help you out. Imagine that I set out the perfect time and place. All you have to say is the phrase that\n"
-            + "will successfully end the relationship. What do you say?\n1.\"I don't like you, we would definitely be better off apart. By the way you\n" +
-            "have a bad taste in music! Good-bye!\"\n2.\"I "
-            + "prefer if we took a break for some time. I will call you but for now, adios...\"\n3.\"I'm not sure if this can work anymore. "
-            + "I want to be alone.\"";
-    //Yes also 3rd situation
-    String attractiveUPAYO12 = "WOAH! I am mindblown! This worked! I am just kidding. Of course it didn't work, this person is\n"
-            + "an actual social media influencer! They called you out publicly and posted 8 times about how terrible you were and how they are depressed\n"
-            + "now. All their fans are coming for you! Your DMs are full of clown emojis! You act like you don't care but really, you are deeply hurt :(";
+    String attAns3 = "Alright, I'll help you out. Imagine that I set out the perfect time and place. All you have to say is the phrase that "
+            + "will successfully end the relationship. What do you say?\n1.\"I don't like you, we would definitely be better off apart. By the way you "
+            + "have a bad taste in music! Good-bye!\"\n2.\"I prefer if we took a break for some time. I will call you but for now, adios...\"\n3.\"I'm "
+            + "not sure if this can work anymore. I want to be alone.\"";
+    
+    String attractiveUPAYO12 = "WOAH! I am mindblown! This worked! I am just kidding. Of course it didn't work, this person is an actual social media "
+            + "influencer! They called you out publicly and posted 8 times about how terrible you were and how they are depressed now. All their "
+            + "fans are coming for you! Your DMs are full of clown emojis! You act like you don't care but really, you are deeply hurt :(";
     String attAns4 = "What do you do to stop this?\n1. Make a public apology and say you were wrong.\n2. Start posting in "
             + "Cuneiform, the earliest known system of writing. \n3. Call you representative or senator and ask for them to vouch for you.";
-    //Yes. Yet another 3rd situation.
+    
     String attractiveUPAYO13 = "Ok, I think that went well. They seem, sad-ish but it's all rad-ish! Hey, this is weird: The person that you\n"
             + "rejected has called you 18 times while you were asleep. Oh no, this seems like a classic case of an obsessive ex. Sorry to tell you,\n"
             + "but it doesn't seem like it's stopping anytime soon. What do you do?\n1. DM them:\"AHHHHHHHH! STOOOOP! please?\"\n2. Meet with them "
             + "and try to reason and say you don't want them to call you anymore.\n3. Tell them you will call the authorities next time they call "
             + "you or contact you in any way.";
-    //All the remaining are third if there are two digits after attractiveUPAYO. The rest will be the endings.
-    String attractiveUPAYO21 = "Hey, that was smart! Usually, asking outright leads to awkward moments and swift rejections, but how can\n"
-            + "anyone reject someone with such good looks? Anyways, they said yes and you took them out to a restaurant called Subway for a\n"
-            + "date. But wait a minute, what is that smell? You realize that you put on the wrong undershirt and now, you stink! Luckily, your date\n"
+    
+    String attractiveUPAYO21 = "Hey, that was smart! Usually, asking outright leads to awkward moments and swift rejections, but how can "
+            + "anyone reject someone with such good looks? Anyways, they said yes and you took them out to a restaurant called Sooubway for a "
+            + "date. But wait a minute, what is that smell? You realize that you put on the wrong undershirt and now, you stink! Luckily, your date "
             + "hasn't noticed yet, but it is only a matter of time. What do you do next?\n1. Keep your distance for the whole date and hope for the "
             + "best.\n2. Excuse yourself and escape through the bathroom window. From there, make a run for the nearest drugstore and buy the best-"
             + "looking perfume.\n3. End the date immediately and run home.(Your personal image is more important than a crush, after all)";
-    String attractiveUPAYO22 = "You know what? That was a sensible choice. However, your \"subtle hints\" did not work and now, your crush has\n"
+    
+    String attractiveUPAYO22 = "You know what? That was a sensible choice. However, your \"subtle hints\" did not work and now, your crush has "
             + "a partner. I guess the lesson we can learn from this is to be direct and brave. ";
     String attAns5 = "How will you get your crush back?\n1. Follow the relationship very closely and make your move when they break up.\n" +
             "2. Sabotage the relationship by spreading false rumors. \n3. Talk to your crush and tell them how you truly feel.";
 
-    String attractiveUPAYO23 = "Woah, woah, woah. Hold your horses! You can't just do that. Your crush is overwhelmed with surprise and\n"
-            + "just leaves without answering your question. You are humiliated. No one thinks of you as an attractive person anymore, they just\n"
+    String attractiveUPAYO23 = "Woah, woah, woah. Hold your horses! You can't just do that. Your crush is overwhelmed with surprise and "
+            + "just leaves without answering your question. You are humiliated. No one thinks of you as an attractive person anymore, they just "
             + "think of you as 'the creep'.";
     String attAns6 = "How will you restore your reputation? \n1. Make a public apology through your social media.\n2. "
             + "Try to justify the situation by saying you were playing truth or dare with your friends and they dared you and you had to do it to\n"
             + "win the game.\n3. Commit wire fraud.";
-    String attractiveUPAYO31 = "This choice does seem like a pretty great one but don't forget, we are talking about the internet. No one has\n"
+    
+    String attractiveUPAYO31 = "This choice does seem like a pretty great one but don't forget, we are talking about the internet. No one has "
             + "forgiven you. You still get clown emojis in your messages. You are fed up with this.";
-    String attAns7 = "What do you do?\n1. Stop using social media for a few weeks.\n2. Try to explain the situation from your perspective.\n3. " +
-            "Start posting cooking tutorials as a distraction. ";
-    String attractiveUPAYO32 = "Hey, this is the correct choice! By posting in cuneiform, you give off weird and confusing vibes and now, no one\n"
-            + "messes with you. However, there is a downside to being so weird. Now, no one thinks of you as the attractive one in their class. They\n"
+    String attAns7 = "What do you do?\n1. Stop using social media for a few weeks.\n2. Try to explain the situation from your perspective.\n3. "
+            + "Start posting cooking tutorials as a distraction. ";
+    
+    String attractiveUPAYO32 = "Hey, this is the correct choice! By posting in cuneiform, you give off weird and confusing vibes and now, no one "
+            + "messes with you. However, there is a downside to being so weird. Now, no one thinks of you as the attractive one in their class. They "
             + "see you as the weird kid and they fear you. No one wants to be your friend. How do you fix this?\n1. Embrace this new weird kid mentality."
-            + "\n2. Attend that party your \"friend\" was begging you to go with. \n3. Transfer schools.";
-    String attractiveUPAYO33 = "Are you trolling right now? What exactly do you plan to accomplish by calling a government representative? They have more\n" +
-            "important problems to deal with! You eventually have to go to school. Also, the principal of your school was notified of this \"waste of precious congressional time\"\n" +
-            "and contacted your parents. Your parents grounded you for a year.";
-    String attAns8 = "After a few months, you learn from your friend about this great party and you hear your crush will be there. The party is at\n"
+            + "\n2. Attend that party your former \"friend\" was begging you to go with. \n3. Transfer schools.";
+    
+    String attractiveUPAYO33 = "Are you trolling right now? What exactly do you plan to accomplish by calling a government representative? They have more "
+            + "important problems to deal with! You eventually have to go to school. Also, the principal of your school was notified of this \"waste of "
+            + "precious congressional time\" and contacted your parents. Your parents grounded you for a year.";
+    String attAns8 = "After a few months, you learn from your friend about this great party and you hear your crush will be there. The party is at "
             + "11:00 PM. Problem is, you are still grounded and your parents still haven't forgotten the incident. How do you escape?\n1. You don't escape. "
             + "You accept your punishment.\n2. Escape through the window and hope your parents don't wake up and check on you.\n3. Join the party "
             + "through Zoom.";
-    //All the endings. I am assuming they will be about a sentence or two each. Or they will be about a paragraph depending on what I decide.
-    String attractiveUPAYO111 = "Wow you handled that so bad, your reputation is ruined.";
-    String attractiveUPAYO112 = "They don't stop calling you, driving you deeper and deeper into madness; the restraining order has not helped.";
-    String attractiveUPAYO113 = "Your partner is upset, but they understand. You part on decent terms.";
+    
+    String attractiveUPAYO111 = "How dare you? Your disregard for other people's emotions makes me mad >:(. Stories of your rudeness have spread and "
+            + "you, my friend, are no longer thought of as an attractive person. You basically ruined your own social life. Think before you speak I guess.";
+    String attractiveUPAYO112 = "That wasn't a bad way to break-up. But you made it sound like you would eventually get back with them. Now, every few "
+            + "days, you get messaged or called, asking if you are ready to get back together. You feel like you are going insane...";
+    String attractiveUPAYO113 = "Smooth talking there. You are finally free! Although, you haven't really achieved the goal of this game, which is to date successfully.";
     String attractiveUPAYO121 = "Though you don't stop everyone, the majority of people see that you have truly had time to reflect, and have stopped the harassment.";
     String attractiveUPAYO122 = "Everyone is really confused; some decide to leave you alone, but most see it as the distraction it is, and continue.";
     String attractiveUPAYO123 = "You called your representative, and they were infuriated that you wasted their time with such trivial matters. You are now on a political blacklist.";
