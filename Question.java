9c3e0877efaeb1502b6890492d80eb49a2694446
CyrusM5 <71396972+CyrusM5@users.noreply.github.com>
2021-01-14 18:31:00 -0500

    Update Question.java

--- Question.java
+++ Question.java
@@ -28,4 +28,20 @@ public class Question
   public void setHealth(int numberOf){
     this.numberOf = numberOf;
   }
+  public Question getQuestion() {
+    Question que = new Question(this.question, this.level);
+    if (this.level==1)
+    {
+      que.level = 2;
+    }
+    else if (this.level==3)
+    {
+      que.level = 3;
+    }
+    else if (this.level > 3 || this.level <= 0)
+    {
+      que.level = 1;
+    }
+    return que;
+  }
 }
