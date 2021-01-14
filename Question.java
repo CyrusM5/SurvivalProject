21cad799a4197392f168fb2495c3e3a13f6e6485
CyrusM5 <71396972+CyrusM5@users.noreply.github.com>
2021-01-14 17:26:15 -0500

    Update Question.java

--- Question.java
+++ Question.java
@@ -2,15 +2,30 @@ public class Question
 {
   private String question;
   private int level;
+  private int numberOf;
   public Question (String q, int l)
   {
     question = q;
     level = l;
   }
+  public Question (int l, int nO)
+  {
+    level = l;
+    numberOf = nO;
+  }
   public String toString(){
     return question + " (level " + level + ") ";
   }
+  public String toTString(){
+    return "you scored " + numberOf + ", level: " + level + " questions. ";
+  }
   public int getLevel(){
     return level;
+  }
+  public int getNumber(){
+    return numberOf;
+  }
+  public void setHealth(int numberOf){
+    this.numberOf = numberOf;
   }
 }
